2973cbbb2282b9d84c990a3a6afbfe4051acd983
Pierre Bajyana <pierre.bajyana@gmail.com>
2022-01-12 16:14:22 +0100

    cleanup

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const optoSigmaScraper = require('./optoSigmaScraper');
-const thorLabsScraper = require('./thorLabsScraper');
+const express = require("express");
+const cors = require("cors");
+const optoSigmaScraper = require("./optoSigmaScraper");
+const thorLabsScraper = require("./thorLabsScraper");
 
-let urlOpto = "https://www.optosigma.com/eu_en/optics/lenses/spherical-lenses/plano-convex-spherical-lenses/spherical-lens-bk7-plano-convex-uncoated-SLB-P.html";
+let urlOpto =
+  "https://www.optosigma.com/eu_en/optics/lenses/spherical-lenses/plano-convex-spherical-lenses/spherical-lens-bk7-plano-convex-uncoated-SLB-P.html";
 let urlThor = "https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=112";
 
 const app = express();
 app.use(cors());
 
-app.get('/api/data', async (request, response) => {
+app.get("/api/data", async (request, response) => {
   const optoData = await optoSigmaScraper(urlOpto);
   const thorData = await thorLabsScraper(urlThor);
   let combinedData = [...optoData, ...thorData];
- 
-  console.log('DATA: ', combinedData);
+
+  console.log("DATA: ", combinedData);
   response.json(combinedData);
-})
+});
 
-
-const PORT = 3001
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`);
 });
--- backend/thorLabsScraper.js
+++ backend/thorLabsScraper.js
@@ -1,106 +1,106 @@
-const puppeteer = require('puppeteer');
-
+const puppeteer = require("puppeteer");
 
 const thorLabsScraper = async (url) => {
-    
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-    const blocked_domains = [
-    'https://pagead2.googlesyndication.com',
-    'https://creativecdn.com',
-    'https://www.googletagmanager.com',
-    'https://cdn.krxd.net',
-    'https://adservice.google.com',
-    'https://cdn.concert.io',
-    'https://z.moatads.com',
-    'https://cdn.permutive.com'
-      ];
-    
-    await page.setRequestInterception(true);
+  const blocked_domains = [
+    "https://pagead2.googlesyndication.com",
+    "https://creativecdn.com",
+    "https://www.googletagmanager.com",
+    "https://cdn.krxd.net",
+    "https://adservice.google.com",
+    "https://cdn.concert.io",
+    "https://z.moatads.com",
+    "https://cdn.permutive.com",
+  ];
 
-    //if the page makes a  request to a resource type of image or stylesheet then abort that request
-    page.on('request', request => {
-        if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || blocked_domains.some(domain => url.includes(domain)))
-            request.abort();
-        else
-            request.continue();
-    });
-    
-    await page.goto(url, {waitUntil: 'domcontentloaded'});
+  await page.setRequestInterception(true);
 
-    let namesUntidy = await page.$$eval('td', (noms) => {
-        return noms.map(x => x.textContent.replace('\n', '').trim())
-    });
+  //if the page makes a  request to a resource type of image or stylesheet then abort that request
+  page.on("request", (request) => {
+    if (
+      request.resourceType() === "image" ||
+      request.resourceType() === "stylesheet" ||
+      blocked_domains.some((domain) => url.includes(domain))
+    )
+      request.abort();
+    else request.continue();
+  });
 
-    let nameswithDuplicates = namesUntidy.filter( item =>  item.startsWith("LA"))
-                           .filter(item => item.length < 7);
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
-    
-    let names = [... new Set(nameswithDuplicates)];
-    
-    const prices = await page.$$eval('td[class="CSS4"][align="right"][style="padding-right: 15px;"]', (prix) => {
-        return prix.map(x => x.textContent.replace('\n', '').trim())
-    })
+  let namesUntidy = await page.$$eval("td", (noms) => {
+    return noms.map((x) => x.textContent.replace("\n", "").trim());
+  });
 
-    let allFeatures = await page.$$eval('td[align="center"][valign="middle"]', (features) => {
-        return features.map(x => x.textContent.replace('\n', '').trim())
-    })
+  let nameswithDuplicates = namesUntidy
+    .filter((item) => item.startsWith("LA"))
+    .filter((item) => item.length < 7);
 
-   let allFeaturesWithoutEmptyStrings = allFeatures.filter(item => item.length > 0)
-                                                    
-   /* Create a multidimensional array with all features corresponding to an index
-     of the array so I can loop afterwards and collect each in their own variable */                                                 
+  let names = [...new Set(nameswithDuplicates)];
 
-   let multidimensionalAllFeatures = allFeaturesWithoutEmptyStrings.reduce((a, c, i) => {
-    return i % 7 === 0 ? a.concat([allFeaturesWithoutEmptyStrings.slice(i, i + 7)]) : a;
-  }, []);
+  const prices = await page.$$eval(
+    'td[class="CSS4"][align="right"][style="padding-right: 15px;"]',
+    (prix) => {
+      return prix.map((x) => x.textContent.replace("\n", "").trim());
+    }
+  );
 
-  
+  let allFeatures = await page.$$eval(
+    'td[align="center"][valign="middle"]',
+    (features) => {
+      return features.map((x) => x.textContent.replace("\n", "").trim());
+    }
+  );
 
-  let diameters = []; let focalLengths = [];
+  let allFeaturesWithoutEmptyStrings = allFeatures.filter(
+    (item) => item.length > 0
+  );
+
+  /* Create a multidimensional array with all features corresponding to an index
+     of the array so I can loop afterwards and collect each in their own variable */
+
+  let multidimensionalAllFeatures = allFeaturesWithoutEmptyStrings.reduce(
+    (a, c, i) => {
+      return i % 7 === 0
+        ? a.concat([allFeaturesWithoutEmptyStrings.slice(i, i + 7)])
+        : a;
+    },
+    []
+  );
+
+  let diameters = [];
+  let focalLengths = [];
   for (let i = 0; i < multidimensionalAllFeatures.length; i++) {
-     diameters.push(multidimensionalAllFeatures[i][0]);
-     focalLengths.push(multidimensionalAllFeatures[i][1]);
+    diameters.push(multidimensionalAllFeatures[i][0]);
+    focalLengths.push(multidimensionalAllFeatures[i][1]);
   }
-
-  
 
   /* Forced manipulations to deal with the messy selector structure of the site. 
      I'm getting rid of 14 products */
-  
-   names.splice(17, 9);
-   prices.splice(17, 9);
-  
-   names.splice(43, 5);
-   prices.splice(43, 5);
 
-   
-  let dataStructure =  names.map(name => {
+  names.splice(17, 9);
+  prices.splice(17, 9);
+
+  names.splice(43, 5);
+  prices.splice(43, 5);
+
+  let dataStructure = names.map((name) => {
     return {
-        'name': name,
-        'provider': 'thorLabs'
-    }
-})
-for (let i = 0; i < dataStructure.length; i++) {
+      name: name,
+      provider: "thorLabs",
+    };
+  });
+  for (let i = 0; i < dataStructure.length; i++) {
     dataStructure[i].price = prices[i];
     dataStructure[i].diameter = diameters[i];
     dataStructure[i].focalLength = focalLengths[i];
-}
+  }
 
+  browser.close();
 
-
-
-
-
-
-
-    browser.close();
-    
-    
-    return dataStructure;
-
+  return dataStructure;
 };
-
 
 module.exports = thorLabsScraper;
